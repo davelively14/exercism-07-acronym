652a684773467601597d17417ae13a8b46e7f20e
Dave Lively <davelively14@gmail.com>
2016-12-13 11:45:45 -0500

    Completed

--- acronym.exs
+++ acronym.exs
@@ -4,6 +4,18 @@ defmodule Acronym do
   "This is a string" => "TIAS"
   """
   @spec abbreviate(String.t()) :: String.t()
-  def abbreviate(string) do
+  def abbreviate(string), do: abbreviate(string |> String.codepoints, "", true)
+  def abbreviate([], result, _primed), do: result
+  def abbreviate([head | tail], result, primed) do
+    cond do
+      primed && head =~ ~r/\p{L}/ ->
+        abbreviate(tail, result <> String.upcase(head), false)
+      head =~ ~r/[ _-]/ ->
+        abbreviate(tail, result, true)
+      head =~ ~r/\p{Lu}/ ->
+        abbreviate(tail, result <> head, false)
+      true ->
+        abbreviate(tail, result, false)
+    end
   end
 end
